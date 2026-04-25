ea5da5020170fbdc8a0f93b8a788ec62f7dcaa0b
Baonn <baboaz132@gmail.com>
2026-04-25 23:21:59 +0700

    week8

--- SourceCode/config.py
+++ SourceCode/config.py
@@ -6,5 +6,5 @@ BATCH_SIZE = 32
 NUM_WORKERS = 0
 NUM_CLASSES = 7  # 7 cảm xúc: Angry, Disgust, Fear, Happy, Sad, Surprise, Neutral
 LR = 0.001
-EPOCHS = 20
+EPOCHS = 30
 DEVICE = "cuda"  # "cuda" hoặc "cpu"
